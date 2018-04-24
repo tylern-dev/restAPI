63e82d7c4d84806861399453e9d63e9dd00ba09e
Tyler Negro <TylerNegro@Tylers-MBP.lan>
2018-04-24 08:35:23 -0600

    logout method working g   🔐

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,16 +1,28 @@
-import React from 'react';
+/* eslint react/jsx-closing-tag-location: 0 */
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
 
 class Navbar extends React.Component {
+  static propTypes = {
+    auth: PropTypes.string.isRequired,
+    handleLogout: PropTypes.func.isRequired,
+  }
   render() {
     return (
       <nav className="navigation">
         <ul>
+
           <NavLink to="/"><h3>Logo</h3></NavLink>
 
-          <NavLink to="/signup">Signup</NavLink>
-          <NavLink to="/login">Login</NavLink>
+          { this.props.auth
+            ? <NavLink to="/" onClick={this.props.handleLogout}>Logout</NavLink>
+            : <Fragment>
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/signup">Signup</NavLink>
+            </Fragment>
+          }
 
         </ul>
       </nav>
--- client/src/containers/NotFound.js
+++ client/src/containers/NotFound.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react';
-import Navbar from '../components/navbar/Navbar';
+
 
 const NotFound = () => (
   <Fragment>
-    <Navbar />
+
     <h1>Oops! It looks like this page does not exist.</h1>
   </Fragment>
 );
